2fe1d71f68af2e88bdec38b5cadfca380764abfb
Nicoleyyyy4 <montanonicole17@gmail.com>
2024-12-13 15:24:18 +0800

    Add files via upload

---
+++ LoginAndSignUpNicole/src/loginandsignupnicole/LoginAndSignUpNicole.java
@@ -0,0 +1,15 @@
+
+package loginandsignupnicole;
+
+
+public class LoginAndSignUpNicole {
+
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
